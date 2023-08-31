52d222ad1dc0a7c235628d1e3e5b127905aa63e8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-08-31 20:59:03 -0300

    Create User.java

---
+++ src/main/java/tech/noetzold/model/User.java
@@ -0,0 +1,21 @@
+package tech.noetzold.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="Usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    @SequenceGenerator(name = "sequence", sequenceName = "sequence", allocationSize = 50)
+    private Integer id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+}
