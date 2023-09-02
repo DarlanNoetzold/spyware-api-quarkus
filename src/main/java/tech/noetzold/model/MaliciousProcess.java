09cbfac1ea7099cc20104bf5bffcaaebc4517f0b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-02 20:23:55 -0300

    Create MaliciousProcess.java

---
+++ src/main/java/tech/noetzold/model/MaliciousProcess.java
@@ -0,0 +1,44 @@
+package tech.noetzold.spyware.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class MaliciousProcess {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    private String nameExe;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MaliciousProcess other = (MaliciousProcess) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
