4f42f3b2d49c0f059499da64b43bdab4ee521514
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-03 09:31:14 -0300

    Create ImageController.java

---
+++ src/main/java/tech/noetzold/controller/ImageController.java
@@ -0,0 +1,69 @@
+package tech.noetzold.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.noetzold.model.Image;
+import tech.noetzold.service.ImageService;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.Collection;
+
+@Path("/image")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ImageController {
+    @Inject
+    ImageService imageService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
+
+    @GET
+    @Path("/getAll")
+    @Transactional
+    public Response getAll(@QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("sortBy") String sortBy) {
+        Collection<Image> images = imageService.findAllImages(page, size, sortBy);
+        if (images.isEmpty()) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+        return Response.ok(images).build();
+    }
+
+    @GET
+    @Path("/get/{id}")
+    @Transactional
+    public Response getImagemById(@PathParam("id") long id) {
+        try {
+            if (id <= 0)
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            Image image = imageService.findImageById(id);
+            if (image == null) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            return Response.ok(image).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @POST
+    @Path("/save")
+    public Response save(Image image) {
+        try {
+            image = imageService.saveImage(image);
+            if (image.getBase64Img() == null) {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+            logger.info("Create " + image.getId());
+            image.setBase64Img("");
+            return Response.status(Response.Status.CREATED).entity(image).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).entity(image).build();
+    }
+}
