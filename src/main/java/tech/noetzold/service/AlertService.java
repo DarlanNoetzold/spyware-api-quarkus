a62b7abe5f8b8e3670cf2994c5db5f1efb72e755
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-03 19:08:58 -0300

    Update AlertService.java

--- src/main/java/tech/noetzold/service/AlertService.java
+++ src/main/java/tech/noetzold/service/AlertService.java
@@ -8,8 +8,6 @@ import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response;
 import tech.noetzold.model.Alert;
 import tech.noetzold.repository.AlertRepository;
 
@@ -22,26 +20,31 @@ public class AlertService {
     @Inject
     AlertRepository alertaRepository;
 
-    @CacheResult(cacheName = "alert")
+    @CacheResult(cacheName = "listalert")
+    @Transactional
     public List<Alert> findAll(int page, int size, String sortBy) {
         Sort sort = Sort.ascending(sortBy);
         PanacheQuery<Alert> query = alertaRepository.findAll(sort);
-        return query.page(Page.of(page, size)).list();
+
+        int offset = (page - 1) * size;
+        return query.page(Page.of(offset, size)).list();
     }
 
     @CacheResult(cacheName = "alert")
+    @Transactional
     public Alert findAlertaById(Long id) {
         Optional<Alert> optionalAlert = alertaRepository.findByIdOptional(id);
-        return optionalAlert.orElseThrow(() -> new WebApplicationException("Alert not found", Response.Status.NOT_FOUND));
+        return optionalAlert.orElse(null);
     }
 
     @CacheResult(cacheName = "alert")
+    @Transactional
     public List<Alert> findAlertaByPcId(String pcId) {
         return alertaRepository.findAllByPcId(pcId);
     }
 
     @Transactional
-    public Alert saveAlerta(Alert alert) {
+    public Alert saveAlert(Alert alert) {
         alertaRepository.persist(alert);
         return alert;
     }
