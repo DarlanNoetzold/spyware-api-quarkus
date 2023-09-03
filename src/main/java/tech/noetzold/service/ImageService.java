53db685e79a382cb094721df39229267e9c2588d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-03 19:09:03 -0300

    Update ImageService.java

--- src/main/java/tech/noetzold/service/ImageService.java
+++ src/main/java/tech/noetzold/service/ImageService.java
@@ -7,8 +7,7 @@ import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response;
+
 import tech.noetzold.model.Image;
 import tech.noetzold.repository.ImageRepository;
 
@@ -20,17 +19,20 @@ public class ImageService {
     @Inject
     ImageRepository imageRepository;
 
-    @CacheResult(cacheName = "image")
+    @CacheResult(cacheName = "listimage")
+    @Transactional
     public List<Image> findAllImages(int page, int size, String sortBy){
         Sort sort = Sort.ascending(sortBy);
         PanacheQuery<Image> query = imageRepository.findAll(sort);
-        return query.page(Page.of(page, size)).list();
-    }
 
+        int offset = (page - 1) * size;
+        return query.page(Page.of(offset, size)).list();
+    }
     @CacheResult(cacheName = "image")
+    @Transactional
     public Image findImageById(Long id){
         Optional<Image> optionalImage = imageRepository.findByIdOptional(id);
-        return optionalImage.orElseThrow(() -> new WebApplicationException("Image not found", Response.Status.NOT_FOUND));
+        return optionalImage.orElse(null);
     }
 
     @Transactional
