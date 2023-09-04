8220ca8dc22df88e9035cb31c5bf2d1fc6d27b26
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-03 22:47:43 -0300

    Update MaliciousPortController.java

--- src/main/java/tech/noetzold/controller/MaliciousPortController.java
+++ src/main/java/tech/noetzold/controller/MaliciousPortController.java
@@ -1,7 +1,6 @@
 package tech.noetzold.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jboss.logging.Logger;
 import tech.noetzold.model.MaliciousPort;
 import tech.noetzold.service.MaliciousPortService;
 
@@ -20,7 +19,7 @@ public class MaliciousPortController {
     @Inject
     MaliciousPortService maliciousPortService;
 
-    private static final Logger logger = LoggerFactory.getLogger(MaliciousPortController.class);
+    private static final Logger logger = Logger.getLogger(MaliciousPortController.class);
 
     @GET
     @Path("/getAll")
@@ -28,8 +27,10 @@ public class MaliciousPortController {
     public Response getAll(@QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("sortBy") String sortBy) {
         Collection<MaliciousPort> maliciousPorts = maliciousPortService.findAllMaliciousPort(page, size, sortBy);
         if (maliciousPorts.isEmpty()) {
+            logger.error("There is no maliciousPort");
             return Response.status(Response.Status.NO_CONTENT).build();
         }
+        logger.info("MaliciousPort returned quantity: " + maliciousPorts.size());
         return Response.ok(maliciousPorts).build();
     }
 
@@ -38,12 +39,15 @@ public class MaliciousPortController {
     @Transactional
     public Response getMaliciousPortById(@PathParam("id") long id) {
         if (id <= 0) {
+            logger.error("Invalid id: " + id);
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
         MaliciousPort maliciousPort = maliciousPortService.findMaliciousPortById(id);
         if (maliciousPort == null) {
+            logger.error("There is no maliciousPort with the id: " + id);
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+        logger.info("MaliciousPort returned: " + id);
         return Response.ok(maliciousPort).build();
     }
 
@@ -51,12 +55,14 @@ public class MaliciousPortController {
     @Path("/save")
     public Response save(MaliciousPort maliciousPort) {
         if (maliciousPort == null || maliciousPort.getVulnarableBanners() == null) {
+            logger.error("Invalid to save maliciousPort: " + maliciousPort);
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
 
         MaliciousPort existingMaliciousPort = maliciousPortService.findMaliciousPortByVulnarableBanners(maliciousPort.getVulnarableBanners());
 
         if (existingMaliciousPort != null) {
+            logger.info("Create MaliciousPort: " + maliciousPort.getVulnarableBanners());
             return Response.ok(existingMaliciousPort).status(Response.Status.CREATED).build();
         }
 
@@ -65,6 +71,7 @@ public class MaliciousPortController {
             logger.info("Create MaliciousPort: " + maliciousPort.getVulnarableBanners());
             return Response.ok(maliciousPort).status(Response.Status.CREATED).build();
         } catch (Exception e) {
+            logger.error("Error to save maliciousPort: " + maliciousPort.getVulnarableBanners());
             e.printStackTrace();
         }
         return Response.status(Response.Status.BAD_REQUEST).entity(maliciousPort).build();
