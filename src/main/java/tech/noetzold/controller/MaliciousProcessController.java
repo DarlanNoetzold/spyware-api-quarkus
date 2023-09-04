2ed24556ff661bc74b0ac94405afbd73eff229e7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-09-03 22:47:47 -0300

    Update MaliciousProcessController.java

--- src/main/java/tech/noetzold/controller/MaliciousProcessController.java
+++ src/main/java/tech/noetzold/controller/MaliciousProcessController.java
@@ -1,7 +1,6 @@
 package tech.noetzold.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jboss.logging.Logger;
 import tech.noetzold.model.MaliciousProcess;
 import tech.noetzold.service.MaliciousProcessService;
 
@@ -19,7 +18,7 @@ public class MaliciousProcessController {
     @Inject
     MaliciousProcessService maliciousProcessService;
 
-    private static final Logger logger = LoggerFactory.getLogger(MaliciousProcessController.class);
+    private static final Logger logger = Logger.getLogger(MaliciousProcessController.class);
 
     @GET
     @Path("/getAll")
@@ -27,8 +26,10 @@ public class MaliciousProcessController {
     public Response getAll(@QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("sortBy") String sortBy) {
         Collection<MaliciousProcess> maliciousProcesses = maliciousProcessService.findAllMaliciousProcess(page, size, sortBy);
         if (maliciousProcesses.isEmpty()) {
+            logger.error("There is no maliciousProcess.");
             return Response.status(Response.Status.NO_CONTENT).build();
         }
+        logger.info("MaliciousProcess returned quantity: " + maliciousProcesses.size());
         return Response.ok(maliciousProcesses).build();
     }
 
@@ -37,12 +38,15 @@ public class MaliciousProcessController {
     @Transactional
     public Response getMaliciousProcessById(@PathParam("id") long id) {
         if (id <= 0) {
+            logger.error("Invalid id: " + id);
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
         MaliciousProcess maliciousProcess = maliciousProcessService.findMaliciousProcessById(id);
         if (maliciousProcess == null) {
+            logger.error("There is no maliciousProcess with id: " + id);
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+        logger.info("MaliciousProcess returned: " + maliciousProcess.getNameExe());
         return Response.ok(maliciousProcess).build();
     }
 
@@ -50,12 +54,14 @@ public class MaliciousProcessController {
     @Path("/save")
     public Response save(MaliciousProcess maliciousProcess) {
         if (maliciousProcess == null) {
+            logger.error("Invalid MaliciousProcess: " + maliciousProcess);
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
 
         MaliciousProcess existingMaliciousProcess = maliciousProcessService.findMaliciousProcessByNameExe(maliciousProcess.getNameExe());
 
         if (existingMaliciousProcess != null) {
+            logger.info("Create MaliciousProcess: " + maliciousProcess.getNameExe());
             return Response.ok(existingMaliciousProcess).status(Response.Status.CREATED).build();
         }
 
