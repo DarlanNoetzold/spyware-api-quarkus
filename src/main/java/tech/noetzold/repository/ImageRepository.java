0db81ebc9ebaa34fefd3992a4a0e8b101e81ccf0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-31 13:35:37 -0300

    Delete ImageRepository.java

--- src/main/java/tech/noetzold/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.repository;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-import tech.noetzold.model.Image;
-
-@ApplicationScoped
-public class ImageRepository implements PanacheRepository<Image> {
-}
