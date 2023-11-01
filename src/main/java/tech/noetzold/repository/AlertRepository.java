6455623b2cde95447f0f55d9bd94375620845b5e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-31 22:53:04 -0300

    Update AlertRepository.java

--- src/main/java/tech/noetzold/repository/AlertRepository.java
+++ src/main/java/tech/noetzold/repository/AlertRepository.java
@@ -1,8 +1,10 @@
 package tech.noetzold.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Page;
 import jakarta.enterprise.context.ApplicationScoped;
 import tech.noetzold.model.Alert;
+import tech.noetzold.model.Company;
 
 import java.util.List;
 
@@ -11,5 +13,11 @@ public class AlertRepository implements PanacheRepository<Alert> {
 
     public List<Alert> findAllByPcId(String pcId) {
         return list("pcId", pcId);
+    }
+
+    public List<Alert> findByCompany(int page, int size, Long companyId) {
+        return find("company.companyId = ?1 order by dataCadastro", companyId)
+                .page(Page.of(page, size))
+                .list();
     }
 }
