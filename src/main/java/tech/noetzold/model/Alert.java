62fed112f85082eb6260549c5c28e8fc53eb7975
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-31 21:28:14 -0300

    Update Alert.java

--- src/main/java/tech/noetzold/model/Alert.java
+++ src/main/java/tech/noetzold/model/Alert.java
@@ -41,6 +41,11 @@ public class Alert implements Serializable {
 
     private String language;
 
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "company")
+    private Company company;
+
     @Override
     public int hashCode() {
         final int prime = 31;
