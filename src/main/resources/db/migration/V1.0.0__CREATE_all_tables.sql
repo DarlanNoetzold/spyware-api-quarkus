d7281bf26b6d2dbb1aa8341f88e34bdd483ad814
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-31 21:28:28 -0300

    Update V1.0.0__CREATE_all_tables.sql

--- src/main/resources/db/migration/V1.0.0__CREATE_all_tables.sql
+++ src/main/resources/db/migration/V1.0.0__CREATE_all_tables.sql
@@ -56,6 +56,24 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.maliciousport
     OWNER to postgres;
 
+-- Table: public.company
+
+-- DROP TABLE IF EXISTS public.company;
+
+CREATE TABLE IF NOT EXISTS public.company
+(
+    registerdate date,
+    id bigint NOT NULL,
+    document character varying(255) COLLATE pg_catalog."default",
+    name character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.company
+    OWNER to postgres;
+
 
 -- Table: public.alert
 
@@ -64,14 +82,19 @@ ALTER TABLE IF EXISTS public.maliciousport
 CREATE TABLE IF NOT EXISTS public.alert
 (
     data_cadastro date NOT NULL,
+    company bigint,
     id bigint NOT NULL,
     language character varying(255) COLLATE pg_catalog."default",
-    pcid character varying(255) COLLATE pg_catalog."default",
     log character varying(255) COLLATE pg_catalog."default",
+    pcid character varying(255) COLLATE pg_catalog."default",
     processos text COLLATE pg_catalog."default",
     image oid,
     models smallint[],
-    CONSTRAINT alert_pkey PRIMARY KEY (id)
+    CONSTRAINT alert_pkey PRIMARY KEY (id),
+    CONSTRAINT fkk167ueux0ac7f5xybf190tgkg FOREIGN KEY (company)
+        REFERENCES public.company (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 
 TABLESPACE pg_default;
